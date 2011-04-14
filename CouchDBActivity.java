667c2ef33f383b4f8ea69c5ed1e9bc414c0e5619
Prayag Narula <prayag.narula@gmail.com>
2011-04-14 05:51:10 -0700

    Initial commit

---
+++ CouchDBActivity.java
@@ -0,0 +1,156 @@
+package cs160.examples.couchdb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.util.Base64;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+public class CouchDBActivity extends Activity{
+    /** Called when the activity is first created. */
+	private static String hostUrl = "http://cs160.iriscouch.com/";
+	private static String username = "cs160";
+	private static String password = "306soda";
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button storeObject = (Button)findViewById(R.id.Button01);
+        Button getObject = (Button)findViewById(R.id.Button02);
+        
+        storeObject.setOnClickListener(this.storeObjectClickListener);
+        getObject.setOnClickListener(this.getObjectClickListener);
+        
+        
+    }
+    
+    private OnClickListener storeObjectClickListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			if(!putJSONObject(hostUrl,"programdatabase/foo"))
+			{
+				Log.e("creation database", "failure");
+			}
+			
+		}
+    };
+		
+	private OnClickListener getObjectClickListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	}; 
+	
+	private Boolean createDatabase(String dbName)
+	{
+		return putJSONObject(this.hostUrl, dbName);
+	}
+	
+	private JSONObject getJSONObject(String hosturl,String databaseName) {
+		try {
+	        HttpGet httpGetRequest = new HttpGet(hostUrl + databaseName);
+	        JSONObject jsonResult = sendCouchRequest(httpGetRequest);
+	        Log.d("Got it:", jsonResult.toString());
+	        return jsonResult;
+	    } 
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+		
+	}
+	
+	private Boolean putJSONObject(String hosturl, String databaseName)
+	{
+		 try {
+		        HttpPut httpPutRequest = new HttpPut(hostUrl + databaseName);
+		        
+
+		        
+			    httpPutRequest.setHeader("Accept", "application/json");
+			    httpPutRequest.setHeader("Content-type", "application/json");
+			    httpPutRequest.setHeader("Authorization","Basic " + (Base64.encodeToString("cs160:306soda".getBytes(), Base64.DEFAULT)).trim());
+			    
+		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		        nameValuePairs.add(new BasicNameValuePair("key10", "value10"));
+		        nameValuePairs.add(new BasicNameValuePair("key11", "value11"));
+		        httpPutRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        JSONObject jsonResult = sendCouchRequest(httpPutRequest);
+
+		        return jsonResult.getBoolean("ok");
+		    } 
+		    catch (Exception e) {
+		        e.printStackTrace();
+		    }
+		    return false;
+	}
+	
+	private static JSONObject sendCouchRequest(HttpUriRequest request) {
+	    try {
+	        HttpResponse httpResponse = (HttpResponse) new DefaultHttpClient().execute(request);
+	        HttpEntity entity = httpResponse.getEntity();
+	        if (entity != null) {
+	            InputStream instream = entity.getContent();
+	            String resultString = convertStreamToString(instream);
+	            instream.close();
+	            JSONObject jsonResult = new JSONObject(resultString);
+
+	            return jsonResult;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	
+	private static void setHeader(HttpRequestBase request)  {
+	    request.setHeader("Accept", "application/json");
+	    request.setHeader("Content-type", "application/json");
+	    request.setHeader("Authorization", "Basic " + Base64.encodeToString((username+":"+password).getBytes(),Base64.DEFAULT));
+	}
+	
+	 private static String convertStreamToString(InputStream is) {
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	        StringBuilder sb = new StringBuilder();
+	 
+	        String line = null;
+	        try {
+	            while ((line = reader.readLine()) != null) {
+	            	sb.append(line + "\n");
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                is.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return sb.toString();
+	    }
+}
